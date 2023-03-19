f57f98e329178b00f0f32be0774644fbb9ba43b6
DarkCode662432 <Ff9371668@gmail.com>
2023-03-19 23:29:22 +0200

    compo version 1.3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 function App() {  
   return (
     <>
-      <h1 className="test">New branch</h1>
-      <p>test</p>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css';
-import App from './App';
+import Home from './pages/home';
+import Html from './pages/html';
+import Css from './pages/css';
+import Js from './pages/javascript';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>,
+    errorElement: <h1>Sorry............</h1>
+  },
+  {
+    path: "/",
+    element: <Home/>,
+    errorElement: <h1>Sorry............</h1>
+  },
+  {
+    path: "/html",
+    element: <Html/>,
+    errorElement: <h1>Sorry............</h1>
+  },
+  {
+    path: "/css",
+    element: <Css/>,
+    errorElement: <h1>Sorry............</h1>
+  },
+  {
+    path: "/javascript",
+    element: <Js/>,
+    errorElement: <h1>Sorry............</h1>
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
